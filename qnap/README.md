f4e23720cbcbd623fcb901a2c48abbd36f96558f
kenstir <kenstir@gmail.com>
2016-08-27 13:35:22 -0400

    Take 2 adding qnap subdir

---
+++ qnap/README.md
@@ -0,0 +1,20 @@
+# pyTivo on QNAP
+
+## Overview
+
+This directory contains the structure necessary to create a QPKG for pyTivo that runs on a QNAP NAS.
+
+## How to build
+
+0. Install Entware-ng on your QNAP per the instructions at https://github.com/Entware-ng/Entware-ng/wiki
+
+1. Download and install the QDK on your QNAP.  See https://wiki.qnap.com/wiki/QPKG_Development_Guidelines for links to the Download and documentation.
+
+2. SSH to your QNAP as the admin user.
+
+3. Create a QPKG environment.
+
+```sh
+cd `getcfg QDK Install_Path -f /etc/config/qpkg.conf`
+git clone https://github.com/kenstir/pytivo.git pyTivo-kenstir
+```
---
+++ qnap/pyTivo.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="pyTivo-kenstir"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+APACHE_ROOT=/share/`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
+case "$1" in
+  start)
+    ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+    if [ "$ENABLED" != "TRUE" ]; then
+        echo "$QPKG_NAME is disabled."
+        exit 1
+    fi
+
+    cd $QPKG_ROOT
+    /opt/bin/python pyTivo.py > /var/log/pytivo.log 2>&1 &
+    echo $! > /var/run/pyTivo.pid
+    ;;
+
+  stop)
+    pid=`cat /var/run/pyTivo.pid`
+    test -n "$pid" && kill $pid
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit 0
